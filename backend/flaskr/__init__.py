6e74128d3eeb79725afa15e2676969e57458c4a5
Emmanuel Evuazeze <evuazeze.emmanuel@gmail.com>
2022-07-07 18:04:02 +0100

    feature: delete question

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -82,6 +82,24 @@ def create_app(test_config=None):
     This removal will persist in the database and when you refresh the page.
     """
 
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(Question.id == question_id).one_or_none()
+
+            if question is None:
+                abort(404)
+
+            question.delete()
+
+            return jsonify(
+                {
+                    'success': True
+                }
+            )
+        except:
+            abort(422)
+
     """
     @TODO:
     Create an endpoint to POST a new question,
@@ -152,6 +170,13 @@ def create_app(test_config=None):
         return (
             jsonify({'success': False, 'error': 404, 'message': 'resource not found'}),
             404
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({'success': False, 'error': 422, 'message': 'unprocessable'}),
+            422
         )
 
     return app
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -72,6 +72,24 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'resource not found')
 
+    def test_delete_question(self):
+        res = self.client().delete('/questions/9')
+        data = json.loads(res.data)
+
+        question = Question.query.filter(Question.id == 9).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(question, None)
+
+    def test_422_if_question_does_not_exist(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
